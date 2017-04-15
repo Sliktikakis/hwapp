d87992850172b4196a8b57dd1394f3074953ae92
Sliktikakis <elie.oriol@orange.fr>
2017-04-15 20:26:38 +0200

    Simple Layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	@sent_mails = SentMail.where(sender_address: @user.email).order('created_at DESC')
-  	@received_mails = SentMail.where(recipient_address: @user.email).order('created_at DESC')
+  	@sent_mails = SentMail.where(sender_address: @user.email).
+  						   where("created_at <= ?", DateTime.now).
+  						   where("created_at >= ?", DateTime.now.beginning_of_day - 30).
+  						   order('created_at DESC')
+  	@received_mails = SentMail.where(recipient_address: @user.email).
+  							   where("created_at <= ?", DateTime.now).
+  						   	   where("created_at >= ?", DateTime.now.beginning_of_day - 30).
+  						   	   order('created_at DESC')
   end
 end
